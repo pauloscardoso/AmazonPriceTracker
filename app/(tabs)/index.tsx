a6d95da427781dde3e999798e56dce0989659f82
pauloscardoso <paulocardoso19@live.com>
2024-10-02 17:59:19 -0300

    chore: init project and search box

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,28 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import React, { useState } from 'react';
+import { Pressable, Text, TextInput, View } from 'react-native';
 
 export default function Home() {
+  const [search, setSearch] = useState('');
+
+  const performSearch = () => {
+    alert(`Searching for ${search}`);
+  };
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
+      <Stack.Screen options={{ title: 'Search' }} />
+      <View className="flex-row gap-3 p-3">
+        <TextInput
+          value={search}
+          onChangeText={(text) => setSearch(text)}
+          placeholder="Search for a product"
+          className="flex-1 rounded border border-gray-300 bg-white p-3"
+        />
+        <Pressable onPress={performSearch} className=" rounded bg-teal-600 p-3">
+          <Text>Search</Text>
+        </Pressable>
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
